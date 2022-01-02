e6be1cf2987d36be9eaa05236248fed7c717d4ac
masayushi <65478781+masayushi@users.noreply.github.com>
2022-01-02 15:43:01 +0800

    設定圖片與彈珠射出的起始點

---
+++ BoomBoomWitch_20211219/Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>
+/// 北╰参
+/// 菲公竚
+/// 祇甮紆痌
+/// 北
+/// </summary>
+public class ControlSystem : MonoBehaviour
+{
+    #region 逆
+    [Header("絙繷")]
+    public GameObject goArrow;
+    [Header("ネΘ紆痌竚")]
+    public Transform traSpawnPoint;
+    [Header("紆痌箇竚")]
+    public GameObject goMarbles;
+    [Header("祇甮硉"), Range(0, 5000)]
+    public float speedShoot = 750;
+    #endregion
+
+    #region ㄆン
+
+    #endregion
+
+    #region よ猭
+
+    #endregion
+
+}
